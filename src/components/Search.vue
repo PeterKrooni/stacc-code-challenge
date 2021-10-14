9474a8debf24d2fb1b79b8d777e27c052b056a49
PeterGillow <peter.gillow.krooni@gmail.com>
2021-10-14 21:11:57 +0200

    basic search bar, and api configuration added

---
+++ src/components/Search.vue
@@ -0,0 +1,45 @@
+<template>
+    <div id="container">
+        <input type="text" id="search-bar" @keyup.enter="search()">
+    </div>
+</template>
+
+
+<script>
+export default{
+    data(){
+        return {
+            api_response: Object
+        }
+    },
+    methods: {
+        async search(pep){
+            //https://stacc-code-challenge-2021.azurewebsites.net/api/pep?name=Knut Arild Hareide
+            //`/api/pep?name=${pep}`
+            pep
+            const res = await fetch ("https://stacc-code-challenge-2021.azurewebsites.net/api/pep?name=Knut%20Arild%20Hareide")
+            const data = await res.json();
+            this.api_response = data;
+            this.$emit('search');
+        },
+    },
+    emits: ['search']
+}
+</script>
+
+<style scoped>
+#container{
+    width: 100%;
+    overflow: hidden;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+input{
+    width: 50%;
+    font-size: 3em;
+    color: rgb(125, 125, 125);
+    background-color: rgb(235, 235, 235);
+    border: none;
+}
+</style>
