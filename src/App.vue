45fb8c71f3517ee9d8adfe2ea9542d5bc25dd6e7
PeterGillow <peter.gillow.krooni@gmail.com>
2021-10-16 19:32:32 +0200

    fixed some CSS responsiveness issues for smaller screens

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,4 @@
 <template>
-  <div id="nav">
-    <!-- might add more pages later
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>-->
-  </div>
   <router-view/>
 </template>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -81,7 +81,7 @@ export default {
   position: absolute;
   bottom: 5%;
 }
-@media screen and (max-width: 620px){
+@media screen and (max-height: 900px){
   #btn-container{
     bottom: 90%;
   }
